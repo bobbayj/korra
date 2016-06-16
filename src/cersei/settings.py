ac9342699fcf7f79129f5d4c8c0a0f2e6cdc1b9b
Daniel Quinn <code@danielquinn.org>
2016-06-16 18:19:02 +0100

    Tweaks to get productions working

--- src/cersei/settings.py
+++ src/cersei/settings.py
@@ -88,6 +88,14 @@ DATABASES = {
     }
 }
 
+if os.getenv("CERSEI_DBUSER") and os.getenv("CERSEI_DBPASS"):
+    DATABASES["default"] = {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("CERSEI_DBNAME", "cersei"),
+        "USER": os.getenv("CERSEI_DBUSER"),
+        "PASSWORD": os.getenv("CERSEI_DBPASS")
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
@@ -117,8 +125,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
+STATIC_ROOT = os.getenv("CERSEI_STATIC_ROOT")
 MEDIA_ROOT = os.getenv("CERSEI_MEDIA_ROOT")
 
+# File Encryption
+
 CERSEI_SALT = bytes(os.getenv("CERSEI_SALT"), "utf-8")
+
